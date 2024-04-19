4068fc5c2552b14072eba416d5ae7ebf9c7ec0dd
Camilo Murcia <camilo281913@gmail.com>
2024-04-18 22:39:02 -0500

    Correccion parcial C:

--- src/main/java/co/edu/eci/cvds/model/CAMILO_MURCIA.java
+++ src/main/java/co/edu/eci/cvds/model/CAMILO_MURCIA.java
@@ -9,10 +9,31 @@ import jakarta.persistence.Table;
 @Table(name = "CAMILO_MURCIA")
 public class CAMILO_MURCIA {
     @Id
-    @Column(name = "#Pregunta")
+    @Column(name = "NUMPREG")
     private int numpreg;
-    @Column(name = "Letra Respuesta")
+    @Column(name = "LETRA")
     private String letra;
-    @Column(name = "Argumento")
+    @Column(name = "ARG")
     private String argumento;
+     
+    public CAMILO_MURCIA(){
+        
+    }
+    public CAMILO_MURCIA(int numpreg, String letra, String argumento){
+        this.argumento = argumento;
+        this.letra = letra;
+        this.numpreg = numpreg;
+    }
+
+    public int getNum(){
+        return this.numpreg;
+    }
+
+public String getLetra(){
+    return this.letra;
+}
+public String getArg(){
+    return this.argumento;
+}
+
 }
